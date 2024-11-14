27b243c8917397460d5fc62f485c972d814cf784
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2024-11-14 18:10:08 -0400

    wip: Finalización del servicio de consumos.

--- backend/src/consumptions/consumptions.module.ts
+++ backend/src/consumptions/consumptions.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConsumptionsService } from './consumptions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConsumptionEntity } from './consumption.entity';
+import { BookingsService } from 'src/bookings/bookings.service';
+import { ProductsService } from 'src/products/products.service';
+import { ProductsModule } from 'src/products/products.module';
+import { BookingsModule } from 'src/bookings/bookings.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([ConsumptionEntity]),
+    BookingsModule,
+    ProductsModule,
+  ],
+  providers: [ConsumptionsService, BookingsService, ProductsService,],
+  exports: [ConsumptionsService],
+})
 export class ConsumptionsModule {}
