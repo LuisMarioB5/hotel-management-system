6dc9d87d06c813a89c84b64011701d0e60b77ca3
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2024-11-27 16:34:20 -0400

    wip: Añadido de seguridad al limitar los intentos de login a tres.

--- backend/src/auth/strategies/local.strategy.ts
+++ backend/src/auth/strategies/local.strategy.ts
@@ -2,18 +2,17 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-import { LoginUserDto } from '../dtos/user.login.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super();
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'username', passwordField: 'password' });
   }
 
-  async validate(login: LoginUserDto): Promise<any> {
-    const user = await this.authService.validateUser(login);
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser({ username, password });
     if (!user) {
-      throw new UnauthorizedException("Credenciales inválidas");
+      throw new UnauthorizedException('Credenciales inválidas');
     }
     return user;
   }
