d0f548f8961b328f11f721bb19e5a15028eab97a
LuisMarioB5 <37335260+LuisMarioB5@users.noreply.github.com>
2024-11-30 19:25:06 -0400

    wip: Finalización de un merge perdido.

--- frontend/src/auth/pages/login.auth.js
+++ frontend/src/auth/pages/login.auth.js
@@ -2,11 +2,7 @@ import { validateJwt } from "../authUtils.js";
 
 export function checkAuthorizationLoginPage() {
     const user = validateJwt();
-<<<<<<< Updated upstream
-=======
-    console.log(user)
-    console.log(user.role)
->>>>>>> Stashed changes
+    
     if(user.role === 'ADMINISTRADOR' || user.role === 'GERENTE'){
         return ['success', 'dashboard.html'];
     } else if(user.role === 'RECEPCIONISTA') {
